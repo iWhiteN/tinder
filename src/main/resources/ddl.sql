b0c743981a58cc2b3d145f61ce7118467af007ba
WhiteN <whiten.ncs@gmail.com>
2020-05-21 22:27:24 +0300

    fix type

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -9,8 +9,8 @@ CREATE TABLE users (
 
 CREATE TABLE likes (
 	id serial NOT NULL,
-	id_users_from serial NOT NULL,
-	id_users_to serial NOT NULL,
+	id_users_from integer NOT NULL,
+	id_users_to integer NOT NULL,
 	type_action varchar(50) NOT NULL,
 	CONSTRAINT likes_pkey PRIMARY KEY (id),
 	CONSTRAINT likes_type_action_check CHECK ((((type_action)::text = 'like'::text) AND ((type_action)::text = 'dislike'::text))),
@@ -22,7 +22,7 @@ CREATE TABLE messages (
 	id serial NOT NULL,
 	message varchar(400) NOT NULL,
 	datetime_send timestamp NOT NULL,
-	id_users serial NOT NULL,
+	id_users integer NOT NULL,
 	CONSTRAINT messages_pkey PRIMARY KEY (id),
 	CONSTRAINT messages_id_users_fkey FOREIGN KEY (id_users) REFERENCES users(id)
 );
